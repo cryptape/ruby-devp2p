2379e2cf4a8c786bd1872f4ac6fb76c3d5e3a8b7
Jan Xie <jan.h.xie@gmail.com>
2016-05-04 15:59:39 +0800

    better test logging

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,9 +9,9 @@ Logging.logger.root.appenders = [
     File.expand_path('../../test.log', __FILE__),
     layout: Logging.layouts.pattern.new(pattern: "%.1l, [%d] %5l -- %c: %m\n")
   ),
-  #Logging.appenders.stdout
+  Logging.appenders.stdout
 ]
-#Logging.logger.root.level = :debug
+Logging.logger.root.level = :debug
 
 def ivget(obj, name)
   obj.instance_variable_get(name)
