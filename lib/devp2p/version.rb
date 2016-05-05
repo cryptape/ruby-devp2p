3ba4c7027e757a9cc22f0db8695f27b9511cd285
Jan Xie <jan.h.xie@gmail.com>
2016-05-05 23:13:23 +0800

    better version string

--- lib/devp2p/version.rb
+++ lib/devp2p/version.rb
@@ -2,5 +2,12 @@
 module DEVp2p
   VERSION = '0.0.1'
 
-  VersionString = "ruby-devp2p #{VERSION}"
+  VersionString = begin
+                    git_describe_re = /^(?<version>v\d+\.\d+\.\d+)-(?<git>\d+-g[a-fA-F0-9]+(?:-dirty)?)$/
+
+                    rev = `git describe --tags --dirty`
+                    m = rev.match git_describe_re
+
+                    ver = m ? "#{m[:version]}+git-#{m[:git]}" : VERSION
+                  end
 end
